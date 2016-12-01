27f080c323074e629f5793e7e1944bf8e5707c3b
ielole <ieloletin@gmail.com>
2016-12-01 12:10:05 -0500

    Set route and methods for getting a book.
    - added before action
    - added methods to books controller
         -set_book
         -show
    - add get#show route to config/routes.js

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,21 @@
 class BooksController < ApplicationController
+  before_action :set_book, only: [:show]
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  # def book_params
+  #   params.require(:book).permit(:title, :author_last_name, :author_first_name, :genre, :rating)
+  # end
+
   def index
     @books = Book.all
 
     render json: @books
+  end
+
+  def show
+    render json: Book.find(params[:id])
   end
 end
