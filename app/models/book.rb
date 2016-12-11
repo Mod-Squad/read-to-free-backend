9541bc334aa354d27d5a6dfce27ec4edb9d7d672
ielole <ieloletin@gmail.com>
2016-12-10 19:23:49 -0500

    Add validations to book model.
    - Validate input for:
      - uniqueness of title
      - presence of:
          - title
          - author last name
          - author first name

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,6 @@
 class Book < ActiveRecord::Base
   has_many :users, through: :reviews
   has_many :reviews, dependent: :destroy
+  validates :title, uniqueness: true
+  validates :title, :author_last_name, :author_first_name, presence: true
 end
