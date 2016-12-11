ca2d75fc05a336bf7a56ad0f351ebea434bdb112
ielole <ieloletin@gmail.com>
2016-12-10 20:03:26 -0500

    Add validations to review model.
    Validate:
      - presence of:
        - comment
        - rating
        - user
        - book
      - uniqueness of user/book pairing
      - rating for:
        - being a number
        - >= 0
        - <= 50

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,9 @@
 class Review < ActiveRecord::Base
   belongs_to :user, inverse_of: :reviews
   belongs_to :book, inverse_of: :reviews
+  validates :comment, :rating, :user, :book, presence: true
+  validates :book, uniqueness: { scope: [:user] }
+  validates :rating, numericality: { only_integer: true }
+  validates :rating, numericality: { greater_than_or_equal_to: 0 }
+  validates :rating, numericality: { less_than_or_equal_to: 50 }
 end
