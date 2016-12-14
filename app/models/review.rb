2307bbf8e741bdd215d8e7aeff9cf674f3cc4cdb
ielole <ieloletin@gmail.com>
2016-12-14 06:17:11 -0500

    Implement non-case-sensitive search method.
    - Added code to remove the case-sensitive parameter when searching for
    a book by its title.
    - Commented out attempt at setting a step value validation for the
    review rating.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ActiveRecord::Base
   belongs_to :book, inverse_of: :reviews
   validates :comment, :rating, :user, :book, presence: true
   validates :book, uniqueness: { scope: [:user] }
+  # validates :rating % 5, numericality: { equal_to: 0 }
   validates :rating, numericality: { only_integer: true }
   validates :rating, numericality: { greater_than_or_equal_to: 0 }
   validates :rating, numericality: { less_than_or_equal_to: 50 }
