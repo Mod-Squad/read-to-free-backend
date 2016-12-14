c6d92c60e463e2de678ce637a2dc358b63d5d79a
ielole <ieloletin@gmail.com>
2016-12-14 17:18:32 -0500

    Alter search method to return book.id.
    - Fix 'create review' from search results issue.
    - Update token in create-review curl request.

--- scripts/create-review.sh
+++ scripts/create-review.sh
@@ -1,11 +1,11 @@
-TOKEN="BAhJIiU2Nzk3MDM4NmQxZTcwODI1N2QxNmY5MTI1YTAzYzc2ZQY6BkVG--b99044d6b2dc55c19e69d0318c2dbbe0c1764926"
+TOKEN="BAhJIiU2ZTg0NzhjNTc2ZWQ3MDQ1ZTY2NjNiY2ZmMGQwNTFhMgY6BkVG--67d37bda7a9ed71734c985e731cbd402f9dc0ed2"
 curl --include --request POST http://localhost:4741/reviews \
   --header "Content-Type: application/json" \
   --header "Authorization: Token token=$TOKEN" \
   --data '{
     "review": {
       "user_id": "1",
-      "book_id": "1",
+      "book_id": "13",
       "comment": "Fun story, believable characters, hot romance descriptions.",
       "rating": "40"
     }
